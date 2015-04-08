b44c0dee4e2553e20310177851a7949efb5a12fe
Mehant Baid <mehantr@gmail.com>
2015-04-07 19:35:02 -0700

    Add tests.

--- exec/java-exec/src/test/java/org/apache/drill/exec/physical/impl/join/TestNestedLoopJoin.java
+++ exec/java-exec/src/test/java/org/apache/drill/exec/physical/impl/join/TestNestedLoopJoin.java
@@ -38,6 +38,11 @@ public class TestNestedLoopJoin extends PlanTestBase {
   private static final String NLJ = "Alter session set `planner.enable_hashjoin` = false; " +
       "alter session set `planner.enable_mergejoin` = false; " +
       "alter session set `planner.enable_nljoin_for_scalar_only` = false; ";
+
+  private static final String DISABLE_NLJ = "Alter session set `planner.enable_hashjoin` = true; " +
+      "alter session set `planner.enable_mergejoin` = true; " +
+      "alter session set `planner.enable_nljoin_for_scalar_only` = true; ";
+
   private static final String SINGLE_NLJ = "alter session set `planner.disable_exchanges` = true; " + NLJ;
 
 
@@ -203,6 +208,78 @@ public class TestNestedLoopJoin extends PlanTestBase {
   }
 
 
+  // simple utility class that tests NLJ and uses HJ/ MJ as baseline query
+  private void testNLJHelper(String query) throws Exception {
+    testBuilder()
+        .optionSettingQueriesForTestQuery(NLJ)
+        .sqlQuery(query)
+        .unOrdered()
+        .sqlBaselineQuery(query)
+        .optionSettingQueriesForBaseline(DISABLE_NLJ)
+        .unOrdered()
+        .go();
+  }
+  @Test
+  public void testExampleQueries() throws Exception {
+
+    // NLJ with expression in 'on' clause
+    String query = "select a.n_nationkey from cp.`tpch/nation.parquet` a join cp.`tpch/region.parquet` b on a.n_regionkey + 1 = b.r_regionkey and a.n_regionkey + 1 = b.r_regionkey";
+    testNLJHelper(query);
+
+    // NLJ with expression in 'where' clause
+    query = "select a.n_nationkey from cp.`tpch/nation.parquet` a , cp.`tpch/region.parquet` b where a.n_regionkey + 1 = b.r_regionkey and a.n_regionkey + 1 = b.r_regionkey";
+    testNLJHelper(query);
+
+    // push expression in join condition
+    query = "select a.n_nationkey from cp.`tpch/nation.parquet` a join cp.`tpch/region.parquet` b " +
+        " on a.n_regionkey + 100  = b.r_regionkey + 200" +      // expressions in both sides of equal join filter
+        "   and (substr(a.n_name,1,3)= 'L1' or substr(a.n_name,2,2) = 'L2') " +  // left filter
+        "   and (substr(b.r_name,1,3)= 'R1' or substr(b.r_name,2,2) = 'R2') " +  // right filter
+        "   and (substr(a.n_name,2,3)= 'L3' or substr(b.r_name,3,2) = 'R3')";
+    testNLJHelper(query);
+
+    // push expression in join condition
+    query = "select a.n_nationkey from cp.`tpch/nation.parquet` a , cp.`tpch/region.parquet` b " +
+        " where a.n_regionkey + 100  = b.r_regionkey + 200" +      // expressions in both sides of equal join filter
+        "   and (substr(a.n_name,1,3)= 'L1' or substr(a.n_name,2,2) = 'L2') " +  // left filter
+        "   and (substr(b.r_name,1,3)= 'R1' or substr(b.r_name,2,2) = 'R2') " +  // right filter
+        "   and (substr(a.n_name,2,3)= 'L3' or substr(b.r_name,3,2) = 'R3')";
+    testNLJHelper(query);
+
+    query = "select count(*) from cp.`tpch/lineitem.parquet` l, cp.`tpch/partsupp.parquet` ps " +
+        " where l.l_partkey = ps.ps_partkey and l.l_suppkey = ps.ps_suppkey";
+    testNLJHelper(query);
+
+    testNLJHelper("select t1.department_description from cp.`department.json` t1, cp.`employee.json` t2 where (cast(t1.department_id as double)) = t2.department_id");
+    testNLJHelper("select t1.full_name from cp.`employee.json` t1, cp.`department.json` t2 where cast(t1.department_id as double) = t2.department_id and cast(t1.position_id as bigint) = t2.department_id");
+    testNLJHelper("select t1.full_name from cp.`employee.json` t1, cp.`department.json` t2 where t1.department_id = t2.department_id and t1.position_id = t2.department_id");
+
+    // NLJ tests with views
+    test("use dfs.tmp");
+    test("create view nation_view_testNLJ_DRILL2715 as select * from cp.`tpch/nation.parquet`;");
+    test("create view region_view_testNLJ_DRILL2715 as select * from cp.`tpch/region.parquet`;");
+
+    testNLJHelper("select n.n_nationkey, n.n_regionkey, r.r_name from region_view_testNLJ_DRILL2715 r , nation_view_testNLJ_DRILL2715 n where r.r_regionkey = n.n_regionkey ");
+    testNLJHelper("select n.n_regionkey, count(*) as cnt from region_view_testNLJ_DRILL2715 r ,  nation_view_testNLJ_DRILL2715 n where r.r_regionkey = n.n_regionkey and n.n_nationkey > 8 group by n.n_regionkey order by n.n_regionkey");
+    testNLJHelper("select n.n_regionkey, count(*) as cnt from region_view_testNLJ_DRILL2715 r join  nation_view_testNLJ_DRILL2715 n on r.r_regionkey = n.n_regionkey and n.n_nationkey > 8 group by n.n_regionkey order by n.n_regionkey");
+
+    test("drop view region_view_testNLJ_DRILL2715");
+    test("drop view nation_view_testNLJ_DRILL2715 ");
+
+    // complex types with NLJ
+    query = "select t1.uid, t1.events, t1.events[0].evnt_id as event_id, t2.transactions, " +
+        "t2.transactions[0] as trans, t1.odd, t2.even from cp.`project/complex/a.json` t1, " +
+        "cp.`project/complex/b.json` t2 where t1.uid = t2.uid";
+    testNLJHelper(query);
+  }
+
+  @Test
+  public void testNLJWithComplexTypes() throws Exception {
+    String query = "select t1.uid, t1.events, t1.events[0].evnt_id as event_id, t2.transactions, " +
+        "t2.transactions[0] as trans, t1.odd, t2.even from cp.`project/complex/a.json` t1, " +
+        "cp.`project/complex/b.json` t2 where t1.uid = t2.uid";
+    testNLJHelper(query);
+  }
   // TPCH SINGLE MODE TESTS WITH NLJ
   private void testTpchNonDistributeNLJ(String fileName) throws Exception {
     String query = getFile(fileName);
