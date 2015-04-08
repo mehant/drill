e1814a3f9dc0f0e625d05e8bfb5f3200ead854c9
Mehant Baid <mehantr@gmail.com>
2015-04-07 18:33:07 -0700

    Update NestedLoopJoinPrel to reflect changes in NestedLoopJoinPOP

--- exec/java-exec/src/main/java/org/apache/drill/exec/planner/physical/NestedLoopJoinPrel.java
+++ exec/java-exec/src/main/java/org/apache/drill/exec/planner/physical/NestedLoopJoinPrel.java
@@ -104,7 +104,7 @@ public class NestedLoopJoinPrel  extends JoinPrel {
 
     buildJoinConditions(conditions, leftFields, rightFields, leftKeys, rightKeys);
 
-    NestedLoopJoinPOP nljoin = new NestedLoopJoinPOP(leftPop, rightPop, conditions, jtype);
+    NestedLoopJoinPOP nljoin = new NestedLoopJoinPOP(leftPop, rightPop);
     return creator.addMetadata(this, nljoin);
   }
 
